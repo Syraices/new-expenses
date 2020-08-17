e6e847eb8b39b337df99e8c99c03ddee7dd1cbc8
Shmulik Raices <sraices@lbs.local>
2020-08-17 14:12:20 -0400

    trying to connect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {MatSelectModule} from '@angular/material/select';
 import { EditComponent } from './edit/edit.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -30,7 +32,8 @@ import {MatInputModule} from '@angular/material/input';
     MatSelectModule,
     MatFormFieldModule,
     FormsModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
